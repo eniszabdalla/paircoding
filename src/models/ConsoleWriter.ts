7cc4d8c6d9214b2e3fe500bd850a7cc819ed8609
Enisz Abdalla <enisz.abdalla@traxretail.com>
2022-12-16 12:42:10 +0100

    removed older project files

--- src/models/ConsoleWriter.ts
+++
@@ -1,8 +0,0 @@
-import { LoggerModel } from "../interfaces/LoggerModel";
-import NodeDateParser from 'node-date-parser';
-
-export class ConsoleWriter implements LoggerModel {
-    public write(message: string) {
-        console.log(`[${new NodeDateParser().parse("Y-m-d H:i:s.V")}] ${message}`);
-    }
-}
